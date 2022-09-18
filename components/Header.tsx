b774125a582b34ce0f2ddb4b32f357c5ab60b161
Rokas <rookas.rudzenskas@gmail.com>
2022-09-18 18:11:50 +0300

    Rokas

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            Rokas
+        </div>
+    );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        Hello world!
+        <Header />
 
     </div>
   )
