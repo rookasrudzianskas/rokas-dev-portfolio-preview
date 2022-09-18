e60ba45cc681300db2ebeb47638fad85233f15fe
Rokas <rookas.rudzenskas@gmail.com>
2022-09-18 18:19:41 +0300

    Rokas

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 type Props = {}
 
 const Header = ({}: Props) => {
     return (
         <header>
-            I am the header
+           <div>
+               <SocialIcon url="https://twitter.com/jaketrent" />
+           </div>
+            <div>
+
+            </div>
         </header>
     );
 };
