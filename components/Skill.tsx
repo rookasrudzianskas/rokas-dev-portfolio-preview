6b7824996acaf87434cd18ab40c545b5703d0c7d
Rokas <rookas.rudzenskas@gmail.com>
2022-09-19 14:55:04 +0300

    Rokas

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,11 +1,31 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 
-type Props = {}
+type Props = {
+    directionLeft?: boolean;
+}
 
-const Skill = ({}: Props) => {
+const Skill = ({directionLeft}: Props) => {
     return (
-        <div>
-            
+        <div className="group relative flex cursor-pointer ">
+            <motion.img
+                initial={{
+                    x: directionLeft ? -200 : 200,
+                    opacity: 0,
+                }}
+                transition={{duration: 1}}
+                whileInView={{
+                    opacity: 1,
+                    x: 0,
+                }}
+                src="https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_96102ac6497377cd53da621075fe828e/sanity.png"
+                className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28  xl:w-32 xl:h-32 filter group-hover:grayscale duration-300 ease-in-out"
+                alt=""/>
+            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full">
+                <div className="flex items-center justify-center h-full">
+                    <p className="text-3xl font-bold text-black opacity-100">100%</p>
+                </div>
+            </div>
         </div>
     );
 };
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -20,7 +20,21 @@ const Skills = ({}: Props) => {
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
             <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skills for proficiency</h3>
 
-            <div className="grid grid-cols-4 gap-5">
+            <div className="grid grid-cols-4 gap-5 mt-36 xl:pt-56">
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
+                <Skill />
                 <Skill />
             </div>
         </motion.div>
