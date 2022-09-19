bf4c6a72dd502b17dca214af2053b56ea0bc27aa
Rokas <rookas.rudzenskas@gmail.com>
2022-09-19 14:11:26 +0300

    Rokas

---
+++ components/Skill.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {}
+
+const Skill = ({}: Props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Skill;
+// by Rokas with ❤️
