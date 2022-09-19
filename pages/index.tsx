eb97f3868e7bde7ccf085b4f1499eecd782c3864
Rokas <rookas.rudzenskas@gmail.com>
2022-09-19 14:09:18 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
+import Skills from "../components/Skills";
 
 const Home: NextPage = () => {
   return (
@@ -26,6 +27,10 @@ const Home: NextPage = () => {
 
         <section id="experience" className="snap-start">
             <WorkExperience />
+        </section>
+
+        <section id="skills" className="snap-start">
+            <Skills />
         </section>
 
     </div>
