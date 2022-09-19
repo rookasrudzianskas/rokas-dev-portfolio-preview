7c3f7159359180d08f4c0bbd6ffed85ec7386de1
Rokas <rookas.rudzenskas@gmail.com>
2022-09-19 11:59:18 +0300

    Rokas

--- components/About.tsx
+++ components/About.tsx
@@ -23,7 +23,22 @@ const About = ({}: Props) => {
                         transition={{
                             duration: 1.2,
                         }}
+                        className="mt-32 md:mt-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
                         alt=""/>
+            <div className="space-y-10 px-0 md:px-10">
+                <h4 className="text-4xl font-semibold">
+                    Here is a <span className="underline decoration-[#F7AB0A]/50">little</span> background
+                </h4>
+                <p className="text-sm">
+                    I'm Rokas. 💯You might also know me as byRookas! I'll be your coach & mentor inside the idiot-proof course, here is a little bit about me...
+                    I've been coding for over 5 years now. As a Full Stack Developer, I've worked both with startups and large corporations to help build & scale
+                    their companies. Along the journey I released my passion existed in helping others excel and pursue their dreams as upcoming developers.
+                    ⭐ With this passion, I have now trained thousand's of developers across the globe. Through live coaching sessions on Youtube, I have accumulated
+                    several MILLION views demonstrating how to apply developer skills in a range of cool builds and challenges. I deliver REAL VALUE by teaching
+                    REAL WORLD projects to help students enter the world of web and app development. You'll get hands-on experiance and learn the skills that are required
+                    to succeed in the real-world in this community. And if that's not enough I have coned most of the applications you have used in your life!
+                </p>
+            </div>
         </div>
     );
 };
