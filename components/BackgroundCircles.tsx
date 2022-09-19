1e8fff89cd89817a531b640df9cdb358c8bd1fc9
Rokas <rookas.rudzenskas@gmail.com>
2022-09-19 11:34:24 +0300

    Rokas

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -11,7 +11,8 @@ const BackgroundCircles = ({}: Props) => {
             }}
             animate={{
                 scale: [1,2,2,3,1],
-                opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0]
+                opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+                borderRadius: ["20%", "20%", "50%", "80%", "20%"],
             }}
             transition={{
                 duration: 2.5,
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Cursor, Typewriter, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
 
 type Props = {}
 
@@ -23,11 +24,30 @@ const Hero = ({}: Props) => {
     return (
         <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
             <BackgroundCircles />
+            <img src="https://avatars.githubusercontent.com/u/38469892?v=4" className="relative rounded-full w-32 h-32 mx-auto object-cover" alt=""/>
+            <div className="z-20">
+                <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
             <h1 >
-                <p>Bro who loves to code.</p>
+                <span className="text-5xl lg:text-6xl font-semibold px-10">Bro who loves to code.</span>
                 {/*<p>{text}</p>*/}
                 <Cursor cursorColor={"#F7AB0A"} />
             </h1>
+
+            <div className="pt-5">
+                <Link href={"#about"}>
+                    <button className="heroButton">About</button>
+                </Link>
+                <Link href={"/experience"}>
+                    <button className="heroButton">Experience</button>
+                </Link>
+                <Link href={"#skills"}>
+                    <button className="heroButton">Skills</button>
+                </Link>
+                <Link href={"#projects"}>
+                    <button className="heroButton">Projects</button>
+                </Link>
+            </div>
+            </div>
         </div>
     );
 };
