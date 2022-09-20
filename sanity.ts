884eb79674ff6b22df698cc850a2e17f6e8cfad1
Rokas <rookas.rudzenskas@gmail.com>
2022-09-20 06:26:36 +0300

    Rokas

---
+++ sanity.ts
@@ -0,0 +1,17 @@
+// lib/sanity.js
+import {createPreviewSubscriptionHook, createCurrentUserHook, createClient} from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
+
+
+export const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    apiVersion: '2021-03-25', // Learn more: https://www.sanity.io/docs/api-versioning
+    useCdn: process.env.NODE_ENV === 'production',
+};
+
+export const sanityClient = createClient(config);
+
+// @ts-ignore
+export const urlFor = (source: any) => createImageUrlBuilder(config).image(source)
+
