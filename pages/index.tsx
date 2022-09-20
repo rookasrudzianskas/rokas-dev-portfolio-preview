19e9c0120682d28c990ebbb11f5725ef162d80d7
Rokas <rookas.rudzenskas@gmail.com>
2022-09-20 07:04:41 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,6 +88,7 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
             skills,
             projects,
             socials
-        }
+        },
+        revalidate: 10,
     }
 }
